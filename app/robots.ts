f2bb5aca809932f673356fc217dcc34a5211550f
ZanTech12 <dontechiel@gmail.com>
2026-09-22 03:56:17 +0100

    sitemap

--- app/robots.ts
+++ app/robots.ts
@@ -1,7 +1,7 @@
 import type { MetadataRoute } from 'next';
 
 export default function robots(): MetadataRoute.Robots {
-  const baseUrl = 'https://zannu.bimtechsolutions.com'; // Replace with your actual domain later
+  const baseUrl = 'https://zannu.bimtechsolutions.com.ng'; // Replace with your actual domain later
 
   return {
     rules: {
--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,7 +1,7 @@
 import type { MetadataRoute } from 'next';
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = 'https://zannu.bimtechsolutions.com'; // Replace with your actual Vercel/Custom domain later
+  const baseUrl = 'https://zannu.bimtechsolutions.com.ng'; // Replace with your actual Vercel/Custom domain later
   
   return [
     {
